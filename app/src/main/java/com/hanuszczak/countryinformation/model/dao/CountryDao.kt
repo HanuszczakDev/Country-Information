8bf01fe816c82a79124e95347ec7b62c6aede9d4
AdamKomputer <HanuszczakDev>
2023-04-12 13:10:32 +0200

    CountryDao

---
+++ app/src/main/java/com/hanuszczak/countryinformation/model/dao/CountryDao.kt
@@ -0,0 +1,20 @@
+package com.hanuszczak.countryinformation.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.hanuszczak.countryinformation.model.entity.CountryEntity
+
+@Dao
+interface CountryDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(countriesArray: Array<CountryEntity>)
+
+    @Update
+    suspend fun update(countryEntity: CountryEntity)
+
+    @Delete
+    suspend fun delete(countryEntity: CountryEntity)
+
+    @Query("SELECT * FROM country ORDER BY official_name ASC")
+    fun getAll(): LiveData<List<CountryEntity>>
+}
