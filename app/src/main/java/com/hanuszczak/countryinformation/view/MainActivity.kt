8a7cae9e56c8e4ce5e8dac93b3afd6379b21bfce
AdamKomputer <HanuszczakDev>
2023-04-12 21:44:58 +0200

    remove VmPolicy

--- app/src/main/java/com/hanuszczak/countryinformation/view/MainActivity.kt
+++ app/src/main/java/com/hanuszczak/countryinformation/view/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.hanuszczak.countryinformation.view
 
 import android.os.Bundle
-import android.os.StrictMode
-import android.os.StrictMode.VmPolicy
 import androidx.appcompat.app.AppCompatActivity
 import com.hanuszczak.countryinformation.R
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        StrictMode.setVmPolicy(
-            VmPolicy.Builder(StrictMode.getVmPolicy())
-                .detectLeakedClosableObjects()
-                .build()
-        )
     }
 }
