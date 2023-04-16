7f570464c33e2aeedc7e64cdbfb32a657bd14680
ahanuszczak <hanyhusajn@gmail.com>
2023-04-16 22:11:25 +0200

    my location enable with localization permissions

--- app/src/main/java/com/hanuszczak/countryinformation/view/maps/MapsFragment.kt
+++ app/src/main/java/com/hanuszczak/countryinformation/view/maps/MapsFragment.kt
@@ -1,19 +1,26 @@
 package com.hanuszczak.countryinformation.view.maps
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.hanuszczak.countryinformation.R
 
 class MapsFragment : Fragment() {
+
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private lateinit var map: GoogleMap
 
     private var latitude = 0f
     private var longitude = 0f
@@ -29,14 +36,47 @@ class MapsFragment : Fragment() {
          * user has installed Google Play services and returned to the app.
          */
 
+        map = googleMap
+
         val zoomLevel = 5f
         val countryLatLng = LatLng(latitude.toDouble(), longitude.toDouble())
 
-//        val sydney = LatLng(latitude.toDouble(), longitude.toDouble())
-
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(countryLatLng, zoomLevel))
-//        googleMap.addMarker(MarkerOptions().position(countryLatLng).title("Marker in Sydney"))
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        enableMyLocation(googleMap)
+    }
+
+    private fun enableMyLocation(googleMap: GoogleMap) {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+        googleMap.isMyLocationEnabled = true
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation(map)
+            }
+        }
     }
 
     override fun onCreateView(
