2230f1fa1bd67c43827ac064d7e98587c0bc30c2
ahanuszczak <hanyhusajn@gmail.com>
2023-04-16 17:20:54 +0200

    Up Button | DetailFragment layout with CollapsingToolbarLayout

--- app/src/main/java/com/hanuszczak/countryinformation/view/MainActivity.kt
+++ app/src/main/java/com/hanuszczak/countryinformation/view/MainActivity.kt
@@ -2,11 +2,26 @@ package com.hanuszczak.countryinformation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.hanuszczak.countryinformation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/hanuszczak/countryinformation/view/detail/DetailFragment.kt
+++ app/src/main/java/com/hanuszczak/countryinformation/view/detail/DetailFragment.kt
@@ -6,8 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hanuszczak.countryinformation.R
+import com.hanuszczak.countryinformation.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
